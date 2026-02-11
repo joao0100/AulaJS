9f4c09e491866daa7bc0914deedb3e706729b8e0
João Gabriel <joao.batista128@etec.sp.gov.br>
2026-02-11 15:10:48 -0300

    11/02

---
+++ Aula01/script.js
@@ -0,0 +1,19 @@
+var nome ="João";
+
+var idade =16;
+
+var altura = 1.65;
+
+var dias = idade * 365;
+
+console.log( nome );
+console.log( idade );
+console.log( altura );
+
+document.write("<h1>Olá, seja bem vindo ao JavaScript</h1>");
+
+document.write( nome + "<br>" );
+document.write( idade + "<br>" );
+document.write( altura + "<br>" );
+
+document.write("<p style= 'color : red' >Você já viveu " + dias + " dias</p>" );
